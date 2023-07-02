bd57132a223e79aad32445633cb144d1b8a70206
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-07-02 12:12:27 -0400

    added react reveal to a icon

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import mountains from '../assets/imgs/mountain.png';
 
 import { AbsoluteCenter } from '@chakra-ui/react';
+import Reveal from 'react-reveal/Reveal';
 
 const Landing = () => {
   return (
     <div className="landing-page">
     <AbsoluteCenter axis='both'>
-        <img 
-            alt='Three mountains with snow on the top of the mountain in the center'
-            src={mountains}
-            style={{filter:`invert(33%) sepia(18%) saturate(1779%) hue-rotate(82deg) brightness(96%) contrast(89%)`}}
-        />
+        <Reveal left>
+            <img 
+                alt='Three mountains with snow on the top of the mountain in the center'
+                src={mountains}
+                style={{filter:`invert(33%) sepia(18%) saturate(1779%) hue-rotate(82deg) brightness(96%) contrast(89%)`}}
+            />
+        </ Reveal>
       </ AbsoluteCenter>
     </div>
   )
