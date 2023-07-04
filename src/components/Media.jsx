5cd119548298ebf491b7a88ceaa63ad1e79cffcc
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-07-04 16:35:25 -0400

    added media componenet

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from './Button';
 
 const Media = () => {
   return (
-    <div>
-      
+    <div className='video-container'>
+        <video src='./assets/videos/video-1.mp4'autoPlay loop muted />
+        <h1>ADVENTURE CALLS</h1>
+        <p>Embrace the Journey</p>
+        <div className="video-btns">
+            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                GET STARTED
+            </Button>
+        </div>
     </div>
   )
 }
