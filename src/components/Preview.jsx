b7366294c5c2f558309ae189f381207032dab4c7
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-07-08 11:58:08 -0400

    added get request to get YT video using YT APi

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const YT_REQUEST = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCDLWkvdzTe9hARN3phcISmg&maxResults=1&order=date&key=${process.env.YT_API_KEY}`;
+// TODO: set conditional: if the video is not available / fails to load display sth else
+
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCDLWkvdzTe9hARN3phcISmg&maxResults=1&order=date&key=${process.env.REACT_APP_YT_API_KEY}`)
+.then((result) => {
+  return result.json();
+}).then((data) => {
+  const video = data.items[0].snippet;
+  console.log(video);
+});
 
 const Preview = () => {
+
+
+  
   return (
-    <div className='preview'>
+    <>
+    {/* <div>
         VIDEO
+    </div> */}
+    <div className='preview'>
+      <img
+        src={video.thumbnails.default}
+      />
     </div>
+    </>
   )
 }
 
